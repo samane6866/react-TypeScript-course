b804465f0907f7c660ca30da69ac7b12b7d20848
samane6866 <samane.bahramian@gmail.com>
2023-08-15 10:54:58 +0200

    handeling from submission

--- practice-react/src/components/Form.tsx
+++ practice-react/src/components/Form.tsx
@@ -1,6 +1,16 @@
 function Form() {
+  // this is also other way for submit function to create seprate function like handel submit and the pass this function to onSubmit function like this :onSubmit(handelSubmit)
+  // const handelSubmit = (event: FormEvent) => {
+  //     event.preventDefault();
+  //     console.log("submitted")
+  // }
   return (
-    <form>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        console.log("submitted");
+      }}
+    >
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
@@ -12,8 +22,10 @@ function Form() {
           Age
         </label>
         <input id="age" type="number" className="form-control" />
-          </div>
-          <button  type="submit"  className="btn btn-primary">Submit</button>
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
     </form>
   );
 }
