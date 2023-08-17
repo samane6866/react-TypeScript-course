604070419e08d2374044991bf2fdb36c9cbee730
samane6866 <samane.bahramian@gmail.com>
2023-08-17 14:45:02 +0200

    exercise form useSate/done

--- practice-react/src/components/FormState.tsx
+++ practice-react/src/components/FormState.tsx
@@ -1,40 +1,51 @@
-import { FormEvent } from "react";
-
-
-
-
+import { FormEvent, useState } from "react";
 
 function FormState() {
-    
-    const handelSubmit = (event: FormEvent) => {
-        
-        event.preventDefault();
-    }
+  // first we create useState hook with our object
+  const [person, setPerson] = useState({ name: "", age: 0 });
 
+  // our inputs will have onChange event that is triggered every time that user types
+  // a keystroke
+  const handelSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(person);
+  };
 
+  return (
+    <form onSubmit={handelSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          id="name"
+          type="text"
+          value={person.name}
+          className="form-control"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, age: parseInt(event.target.value) })
+          }
+          id="age"
+          type="number"
+          className="form-control"
+          value={person.age}
+        />
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+}
 
-
-    return (
-      <form
-        onSubmit={handelSubmit}
-      >
-        <div className="mb-3">
-          <label htmlFor="name" className="form-label">
-            Name
-          </label>
-          <input  id="name" type="text" className="form-control" />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="age" className="form-label">
-            Age
-          </label>
-          <input id="age" type="number" className="form-control" />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    );
-  }
-  
-  export default FormState;
+export default FormState;
