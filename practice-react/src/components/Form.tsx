179f659e8e6ffeb3a3342dcc80855674943cee0b
samane6866 <samane.bahramian@gmail.com>
2023-08-17 13:59:03 +0200

    exercise useRef/form done

--- practice-react/src/components/Form.tsx
+++ practice-react/src/components/Form.tsx
@@ -1,5 +1,15 @@
+import { useRef } from "react";
+
+
+// in this point we are using useRef hook for accessing input field ,first we will 1-import our useRef
+// 2-we introduce our useRef that already  stored in useName varieble to the desired input in ths case is : input -name.
+// 3 for accesing to value of our input we 
+
 function Form() {
-  // this is also other way for submit function to create seprate function like handel submit and the pass this function to onSubmit function like this :onSubmit(handelSubmit)
+  let useName = useRef<HTMLInputElement>(null);
+  let useAge = useRef<HTMLInputElement>(null);
+  const person={name:"", age:0}
+  // this is also other way for submit function to create other function like handel submit and the pass this function to onSubmit function like this :onSubmit(handelSubmit)
   // const handelSubmit = (event: FormEvent) => {
   //     event.preventDefault();
   //     console.log("submitted")
@@ -8,20 +18,25 @@ function Form() {
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        console.log("submitted");
+        // we had an error on useName.current, that saying useName.current is probably null, for this reason we are checking before with if conditional, if useName.current isnt null..
+         if(useName.current!==null)
+           person.name=useName.current.value;
+        if (useAge.current !== null)
+          person.age = parseInt(useAge.current.value);
+        console.log(person);
       }}
     >
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input ref={useName} id="name" type="text" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input ref={useAge} id="age" type="number" className="form-control" />
       </div>
       <button type="submit" className="btn btn-primary">
         Submit
